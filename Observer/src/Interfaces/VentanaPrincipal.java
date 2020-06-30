aac93ee3cf29e020b75d9218947909cc966789b4
Carlos <carlosqa73@hotmail.com>
2020-06-30 18:33:18 -0500

    Cambio en la interfaz

--- Observer/src/Interfaces/VentanaPrincipal.java
+++ Observer/src/Interfaces/VentanaPrincipal.java
@@ -5,30 +5,49 @@ import java.util.Observable;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class VentanaPrincipal extends Observable{
 	
-	private HBox root;
+	private VBox root;
+	private HBox h;
+	private HBox h2;
+	private TextField tf;
 	private Button boton1;
 	private Button boton2;
 	private Button boton3;
+	private Button boton4;
 	
 	public VentanaPrincipal() {
 		
-		root = new HBox();
+		root = new VBox();
+		h = new HBox();
+		h2 = new HBox();
+		tf = new TextField();
 		boton1 = new Button("Azul");
 		boton2 = new Button("Amarillo");
 		boton3 = new Button("Rojo");
+		boton4 = new Button("Imprimir");
 		
-		root.getChildren().addAll(boton1, boton2, boton3);
-		root.setSpacing(50);
+		ejecutarEvento();
+		
+		h.getChildren().addAll(boton1, boton2, boton3);
+		h.setSpacing(50);
+		h.setAlignment(Pos.CENTER);
+		
+		h2.getChildren().addAll(boton4, tf);
+		h2.setAlignment(Pos.CENTER);
+		h2.setSpacing(50);
+		
+		root.getChildren().addAll(h,h2);
 		root.setAlignment(Pos.CENTER);
+		root.setSpacing(40);
 		root.setPadding(new Insets(100,100,100,100));
-		ejercutarEvento();
 		
 	}
-	public void ejercutarEvento() {
+	public void ejecutarEvento() {
 		boton1.setOnMouseClicked(e -> {
 		root.setStyle("-fx-background-color:blue");
 		setChanged();
@@ -51,7 +70,7 @@ public class VentanaPrincipal extends Observable{
 	}
 	
 	
-	public HBox getRoot() {
+	public VBox getRoot() {
 		return root;
 	}
 	
